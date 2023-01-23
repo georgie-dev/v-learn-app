3e6f342a01625527d9613383846ae7e7959c669b
BaByHaRdEn <mangergeorgepraise@gmail.com>
2023-01-23 16:38:04 +0100

    feat:mobile reponsiveness

--- src/modules/dashboard/pages/Assignments.jsx
+++ src/modules/dashboard/pages/Assignments.jsx
@@ -17,7 +17,7 @@ const Assignments = () => {
         <div className='mt-12 w-full '>
         <table className='mx-auto w-full rounded-md border'>
             <thead>
-              <tr className='text-black text-lg dark:text-gray-300 border p-2 rounded-md'>
+              <tr className='text-black text-sm lg:text-lg  dark:text-gray-300 border p-2 rounded-md'>
                   <th className='lg:px-16 font-bold font-rokkitt' scope="col">Course Code</th>
                   <th className='lg:px-16 font-bold font-rokkitt' scope="col">Lecturer</th>
                   <th className='lg:px-16 font-bold font-rokkitt' scope="col">Due Date</th>
@@ -26,22 +26,22 @@ const Assignments = () => {
             </thead>
             <tbody className='mt-10'>
               {select.map((data)=>(
-                <tr className=' border-b p-4 rounded-lg text-slate-900 odd:bg-gray-200 mt-12  odd:dark:bg-slate-500 dark:text-gray-200'>
-                  <td className='lg:px-16 p-3 font-rokkitt font-semibold'>SEN 102</td>
-                  <td className='lg:px-16 p-3 font-rokkitt font-semibold'>Bernard Ephraim</td>
-                  <td className='lg:px-16 p-3 font-rokkitt font-semibold'> 12/02/2023</td>
-                  <td className='lg:px-16 p-3 font-rokkitt font-semibold flex gap-2'>
+                <tr key={data} className=' border-b p-4 rounded-lg text-slate-900 odd:bg-gray-200 mt-12  odd:dark:bg-slate-500 dark:text-gray-200'>
+                  <td className='lg:px-16 p-3 text-sm lg:text-lg font-rokkitt font-semibold'>SEN 102</td>
+                  <td className='lg:px-16 p-3 text-sm lg:text-lg font-rokkitt font-semibold'>Bernard Ephraim</td>
+                  <td className='lg:px-16 p-3 text-sm lg:text-lg font-rokkitt font-semibold'> 12/02/2023</td>
+                  <td className='lg:px-16 p-3 text-sm lg:text-lg font-rokkitt font-semibold flex gap-2'>
                     <button
                     type='button'
                     className= ' bg-sky-700 px-4 py-1 gap-1 rounded-full flex text-white font-bold items-center hover:bg-sky-900'
                     >
-                      <AiOutlineCloudDownload/>Download
+                      <AiOutlineCloudDownload/><span className='hidden lg:flex'>Download</span>
                     </button>
                     <button
                     type='button'
                     className= ' bg-red-700 px-4 py-1 gap-1 rounded-full flex text-white font-bold items-center hover:bg-red-900'
                     >
-                      <AiOutlineCloudUpload/>Submit
+                      <AiOutlineCloudUpload/><span className='hidden lg:flex'>Submit</span>
                     </button>
                   </td>
                 </tr>
--- src/modules/dashboard/pages/Classes.jsx
+++ src/modules/dashboard/pages/Classes.jsx
@@ -5,11 +5,11 @@ const Classes = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl  dark:bg-secondary-dark-bg">
     <Header category='App' title="Classes" />
-    <div className='p-5 h-auto sm:flex md:flex gap-4 mt-12'>
-      <div className='uppercase border text-slate-400 border-slate-400 rounded-xl w-3/4 h-64 justify-center flex items-center bg-main-bg'>
+    <div className='p-5 h-auto block lg:flex md:flex gap-4 mt-12'>
+      <div className='uppercase border text-slate-400 border-slate-400 rounded-xl w-full mb-6 lg:mb-0 lg:w-3/4 h-64 justify-center flex items-center bg-main-bg'>
         Video calling api section
       </div>
-      <div className=' w-1/4 flex flex-col gap-4'>
+      <div className=' lg:w-1/4 flex flex-col gap-4'>
         <textarea placeholder='Write down lecture Notes here..' className='h-full p-2 font-rokkitt rounded-lg dark:bg-slate-400 bg-main-bg dark:placeholder:text-white placeholder:text-slate-400'></textarea>
         <button className='rounded-full bg-sky-600 text-white p-2 w-20'>Save</button>
       </div>
