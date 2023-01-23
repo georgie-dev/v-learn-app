29d3ff6770ebaaf445c7c00956e3d9749dce0f93
BaByHaRdEn <mangergeorgepraise@gmail.com>
2023-01-23 16:28:36 +0100

    feat: trial timetabel library and dark mode feat

--- src/modules/dashboard/pages/CourseMaterials.jsx
+++ src/modules/dashboard/pages/CourseMaterials.jsx
@@ -12,11 +12,11 @@ const CourseMaterials = () => {
 <div className=' w-full mt-12 p-2 h-auto flex flex-wrap gap-3 items-center justify-center'>
 
    {select.map((items)=>(
-      <div className=' text-gray-200 w-80 md:w-56 lg:w-80 mt-4 h-48 rounded-2xl p-4 pt-9 drop-shadow-2xl bg-white shadow-sm dark:bg-main-dark-bg relative'>
-        <div  className=' bg-cm opacity-20 bg-no-repeat bg-cover bg-center absolute left-0 top-0 w-full h-full'></div>
+      <div key={items.id} className=' text-gray-200 w-80 md:w-56 lg:w-80 mt-4 h-48 rounded-2xl p-4 pt-9 drop-shadow-2xl bg-white shadow-sm dark:bg-main-dark-bg relative'>
+        <div className=' bg-cm opacity-20 bg-no-repeat bg-cover bg-center absolute left-0 top-0 w-full h-full'></div>
         <div className='flex flex-col gap-3 text-black dark:text-gray-200 px-4 items-center justify-center '>
-          <p className='text-2xl font-Machina font-extrabold relative'>{items.courseCode}</p>
-          <p className='font-Machina text bold relative'>{items.courseTitle}</p>
+          <p key={items.courseCode} className='text-2xl font-Machina font-extrabold relative'>{items.courseCode}</p>
+          <p key={items.courseTitle} className='font-Machina text bold relative'>{items.courseTitle}</p>
         </div>
         <Link className='float-right bg-secondary-dark-bg font-Machina px-4 p-2 absolute bottom-2 right-2 rounded-full hover:bg-main-dark-bg'> View</Link>
       </div>
--- src/modules/dashboard/pages/Timetable.jsx
+++ src/modules/dashboard/pages/Timetable.jsx
@@ -1,14 +1,57 @@
-import React, { useState } from 'react';
+import React from 'react';
+import '@mobiscroll/react/dist/css/mobiscroll.min.css';
+import { Eventcalendar, getJson, toast, localeEnGB } from '@mobiscroll/react';
+import { useState, useCallback, useEffect, useMemo } from 'react';
 import { Header } from '../components';
 
+function Timetable() {
 
-const Timetable = () => {
+    const [myEvents, setEvents] = useState([]);
+  
+  
 
-  return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl  dark:bg-secondary-dark-bg">
-      <Header category="Tools" title="Timetable" />
-    </div>
-  );
-};
+
+    useEffect(() => {
+        getJson('https://trial.mobiscroll.com/events/?vers=5', (events) => {
+            setEvents(events);
+        }, 'jsonp');
+    }, []);
+    
+    const onEventClick = useCallback((event) => {
+        toast({
+            message: event.event.title
+        });
+    }, []);
+
+    const view = useMemo(() => {
+        return {
+            calendar: { 
+              popover: true, 
+              count: true }
+        };
+    }, []);
+
+    return (
+      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl  dark:bg-secondary-dark-bg">
+      <Header category='Tools' title="Timetable" />
+
+      <Eventcalendar
+            locale={localeEnGB}
+            theme="ios" 
+            themeVariant= 'auto'
+            clickToCreate={true}
+            dragToCreate={true}
+            dragToMove={true}
+            dragToResize={true}
+            eventDelete={true}
+            data={myEvents}
+            view={view}
+            onEventClick={onEventClick}
+       />
+      </div>
+    ); 
+}
+
+
 
 export default Timetable;
