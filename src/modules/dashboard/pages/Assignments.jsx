c2b749da2eefd288d90b7607a454adc6b83d0d3b
BaByHaRdEn <mangergeorgepraise@gmail.com>
2023-01-24 07:52:59 +0100

    refactor: code refactor

--- src/modules/dashboard/pages/Assignments.jsx
+++ src/modules/dashboard/pages/Assignments.jsx
@@ -21,16 +21,15 @@ const Assignments = () => {
                   <th className='lg:px-16 font-bold font-rokkitt' scope="col">Course Code</th>
                   <th className='lg:px-16 font-bold font-rokkitt' scope="col">Lecturer</th>
                   <th className='lg:px-16 font-bold font-rokkitt' scope="col">Due Date</th>
-                  <th className='lg:px-16 font-bold font-rokkitt' scope="col">Submit/Download</th>
               </tr>
             </thead>
             <tbody className='mt-10'>
               {select.map((data)=>(
                 <tr key={data} className=' border-b p-4 rounded-lg text-slate-900 odd:bg-gray-200 mt-12  odd:dark:bg-slate-500 dark:text-gray-200'>
-                  <td className='lg:px-16 p-3 text-sm lg:text-lg font-rokkitt font-semibold'>SEN 102</td>
-                  <td className='lg:px-16 p-3 text-sm lg:text-lg font-rokkitt font-semibold'>Bernard Ephraim</td>
-                  <td className='lg:px-16 p-3 text-sm lg:text-lg font-rokkitt font-semibold'> 12/02/2023</td>
-                  <td className='lg:px-16 p-3 text-sm lg:text-lg font-rokkitt font-semibold flex gap-2'>
+                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-rokkitt font-semibold'>SEN 102</td>
+                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-rokkitt font-semibold'>Bernard Ephraim</td>
+                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-rokkitt font-semibold'> 12/02/2023</td>
+                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-rokkitt font-semibold flex gap-2'>
                     <button
                     type='button'
                     className= ' bg-sky-700 px-4 py-1 gap-1 rounded-full flex text-white font-bold items-center hover:bg-sky-900'
