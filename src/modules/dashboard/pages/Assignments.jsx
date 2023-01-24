69f81e32b6f86c3fb166c25144ccd43d637dbe95
BaByHaRdEn <mangergeorgepraise@gmail.com>
2023-01-24 07:54:59 +0100

    refactor: code refactor

--- src/modules/dashboard/pages/Assignments.jsx
+++ src/modules/dashboard/pages/Assignments.jsx
@@ -18,18 +18,18 @@ const Assignments = () => {
         <table className='mx-auto w-full rounded-md border'>
             <thead>
               <tr className='text-black text-sm lg:text-lg  dark:text-gray-300 border p-2 rounded-md'>
-                  <th className='lg:px-16 font-bold font-rokkitt' scope="col">Course Code</th>
-                  <th className='lg:px-16 font-bold font-rokkitt' scope="col">Lecturer</th>
-                  <th className='lg:px-16 font-bold font-rokkitt' scope="col">Due Date</th>
+                  <th className='lg:px-16 font-bold font-display' scope="col">Course Code</th>
+                  <th className='lg:px-16 font-bold font-display' scope="col">Lecturer</th>
+                  <th className='lg:px-16 font-bold font-display' scope="col">Due Date</th>
               </tr>
             </thead>
             <tbody className='mt-10'>
               {select.map((data)=>(
                 <tr key={data} className=' border-b p-4 rounded-lg text-slate-900 odd:bg-gray-200 mt-12  odd:dark:bg-slate-500 dark:text-gray-200'>
-                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-rokkitt font-semibold'>SEN 102</td>
-                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-rokkitt font-semibold'>Bernard Ephraim</td>
-                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-rokkitt font-semibold'> 12/02/2023</td>
-                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-rokkitt font-semibold flex gap-2'>
+                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-display '>SEN 102</td>
+                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-display '>Bernard Ephraim</td>
+                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-display '> 12/02/2023</td>
+                  <td className='lg:px-16 p-3 text-xs lg:text-lg font-display  flex gap-2'>
                     <button
                     type='button'
                     className= ' bg-sky-700 px-4 py-1 gap-1 rounded-full flex text-white font-bold items-center hover:bg-sky-900'
