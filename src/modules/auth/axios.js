641572cccf65865a24c87c0503b0b5cd15e21a8e
georgie-dev <mangergeorgepraise@gmail.com>
2024-06-20 18:11:30 +0100

    Updated

--- src/modules/auth/axios.js
+++ src/modules/auth/axios.js
@@ -7,7 +7,7 @@ const cookie = new Cookies();
 const token = cookie.get("token");
 
 const axiosInstance= axios.create({
-    baseURL :process.env.BASE_URL,
+    baseURL : process.env.BASE_URL,
     headers: {
         "Content-Type": "application/json",
         Authorization: token !== undefined ? `Bearer ${token}` : "",
